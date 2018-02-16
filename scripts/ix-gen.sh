3fbf717d7f819da3b5ca01e6c7fcef16ad9c9ee3
Marian Marinov <mm@yuhu.biz>
2018-02-16 03:11:31 +0200

    scripts/ix-gen.sh: Generate bgp configuration for the connection to the IX routers
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

---
+++ scripts/ix-gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+max_port=22
+
+if [ "$1" != 22 ]; then
+    max_port=$1
+fi
+
+for i in $(seq 1 $max_port); do
+	if (( $i < 10 )); then
+		a=0$i
+	else
+		a=$i
+	fi
+	echo "
+protocol bgp client$i from PEERS {
+        neighbor 10.0.57.$i as 655$a;
+        source address 10.0.57.254;
+}
+"
+done
