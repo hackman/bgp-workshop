61c831b2d965bedd174ec278bd29b6fa4c8e55c8
Marian Marinov <mm@yuhu.biz>
2019-03-04 22:00:58 +0200

    scripts/bgp-generator.sh: Added support for the second router - set the default max_port to 40 - introduced usage - the configuration now is generated directly with bfd disabled, so we can enable it when anyone is ready
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- scripts/bgp-generator.sh
+++ scripts/bgp-generator.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 s=0
-max_port=22
+prefix=56
+max_port=40
 
-if [ "$1" == 22 ]; then
-    max_port=$1
+if [[ $# -ne 1 ]] && [[ $# -ne 2 ]]; then
+	echo "Usage: $0 tom|jerry [max ports]"
+	exit
 fi
 
-for i in $(seq -w 1 $max_port); do
+if [[ $1 == 'tom' ]]; then
+	prefix=55
+fi
+
+if [[ -n $2 ]]; then
+    max_port=$2
+fi
+
+for as in $(seq -w 1 $max_port); do
 	let a=$s+2
 	let v6=$p+2
-	if (( $i < 10 )); then
-		as=0$i
-	else
-		as=$i
+	if [[ $max_port -lt 10 ]]; then
+		as=$(printf "%02d" $as)
 	fi
 	echo "
-protocol bgp client$i from PEERS {
-        neighbor 10.0.55.$a as 655$as;
-        source address 10.0.55.$(($s+1));
-        export filter client_filter;
+protocol bgp isp_cli_$as from PEERS {
+	neighbor 10.0.${prefix}.$a as 655$as;
+	source address 10.0.${prefix}.$(($s+1));
+#	bdf;
 }
 "
 	let s+=4
