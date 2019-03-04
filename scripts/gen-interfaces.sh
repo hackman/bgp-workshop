79b5ab9b14f792233d4ab5566ad2c99000e4b648
Marian Marinov <mm@yuhu.biz>
2019-03-04 21:52:51 +0200

    scripts/gen-interfaces.sh: Added v6 support
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- scripts/gen-interfaces.sh
+++ scripts/gen-interfaces.sh
@@ -3,9 +3,13 @@
 case "$1" in
 	tom)
 		net=10.0.55
+		net6=2001:67c:21bc:8000::1:
+		prefix=3
 	;;
 	jerry)
-		net=10.0.66
+		net=10.0.56
+		net6=2001:67c:21bc:8001::1:
+		prefix=4
 	;;
 	*)
 		echo "Usage: $0 tom|jerry"
@@ -27,7 +31,16 @@ fi
 for i in $(seq -w 1 $max_port); do
 	a=$s
 	let a++
-	echo -e "auto eth$eth\niface eth$eth inet static\n\taddress ${net}.$a\n\tnetmask 255.255.255.252\n"
+	echo -e "
+auto eth${prefix}$i
+iface eth${prefix}$i inet static
+	address ${net}.$a
+	netmask 255.255.255.252
+
+iface eth${prefix}$i inet6 static
+	address ${net6}:$a
+	prefix  126
+"
 	let s+=4
 	let eth++
 done
