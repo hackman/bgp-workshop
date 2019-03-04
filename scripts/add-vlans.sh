8d0fe5d27b15ace72fe28df24aa2f8334e492af2
Marian Marinov <mm@yuhu.biz>
2019-03-04 21:51:06 +0200

    scripts/add-vlans.sh: Added $out_dev var and set the default port number to 40
    The script still doesn't handle correctly ports bellow 10 :)
    
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- scripts/add-vlans.sh
+++ scripts/add-vlans.sh
@@ -2,17 +2,16 @@
 # This script is used to assign all vlans to their respective containers
 
 # Add the individual vlans from clients
-iface=10
-max_port=22
-if [ "$1" != 22 ]; then
+max_port=40
+out_dev=eth0
+if [[ -n $1 ]]; then
     max_port=$1
 fi
 for i in $(seq -w 1 $max_port); do
-        lxc config device add tom   eth$iface nic nictype=macvlan parent=eth1.4$i name=eth$iface
-        lxc config device add jerry eth$iface nic nictype=macvlan parent=eth1.3$i name=eth$iface
-        let iface++
+	lxc config device add tom   eth0.3$i nic nictype=macvlan parent=${out_dev}.3$i name=eth0.3$i
+	lxc config device add jerry eth0.4$i nic nictype=macvlan parent=${out_dev}.4$i name=eth0.4$i
 done
 
 # Add the VLANs for both Internet Exchanges
-lxc config device add ix1 eth2 nic nictype=macvlan parent=eth1.500 name=eth2
-lxc config device add ix2 eth2 nic nictype=macvlan parent=eth1.600 name=eth2
+lxc config device add ix1 eth2 nic nictype=macvlan parent=${out_dev}.500 name=eth2
+lxc config device add ix2 eth2 nic nictype=macvlan parent=${out_dev}.600 name=eth2
